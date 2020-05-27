b17b4028ea6859cfe46a8c8e2910f12a80693e0e
mhbahmani <mhbahmani79@gmail.com>
2020-05-28 03:51:25 +0430

    fix misspellings at install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,11 @@
 data_backups_dir="$HOME/.local/share/erate"
-dollar_price_file="$data_backups_dir/dollor.txt"
+dollar_price_file="$data_backups_dir/dollar.txt"
 euro_price_file="$data_backups_dir/euro.txt"
 
 rm -rf $data_backups_dir
 mkdir $data_backups_dir
 touch $dollar_price_file
 touch $euro_price_file
-
-chmod 646 $dollar_price_file
-chmod 646 $euro_price_file
 
 erate_script=$(wget -q -O- https://raw.githubusercontent.com/mhbahmani/erate/master/erate)
 
