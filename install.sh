42a83ac1a877b88184389c63fc9cd935664585f9
mhbahmani <mhbahmani79@gmail.com>
2020-05-28 03:08:50 +0430

    change install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,22 @@
 data_backups_dir="$HOME/.local/share/erate"
-dollor_price_file="$data_backups_dir/dollor.txt"
+dollar_price_file="$data_backups_dir/dollor.txt"
 euro_price_file="$data_backups_dir/euro.txt"
 
-sudo mkdir $date_backup_dir
-sudo touch $dollar_price_file
-sudo touch $euro_price_file
+rm -rf $data_backups_dir
+mkdir $data_backups_dir
+touch $dollar_price_file
+touch $euro_price_file
 
-sudo chmod 646 $dollar_price_file
-sudo chmod 646 $euro_price_file
+chmod 646 $dollar_price_file
+chmod 646 $euro_price_file
 
-erate_scripts=$(wget -q -O- https://raw.githubusercontent.com/mhbahmani/erate/master/erate)
+erate_script=$(wget -q -O- https://raw.githubusercontent.com/mhbahmani/erate/master/erate)
 
 main_script_file=/usr/local/bin/erate
 sudo rm -rf $main_script_file
 
-sudo tee << EOF > $main_script_file
-$erate_scripts
+sudo tee $main_script_file << EOF > /dev/null
+$erate_script 
 EOF
 
 sudo chmod +x $main_script_file
