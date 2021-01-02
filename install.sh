2f649a2e48b8dc3ec48bab7f22beb146a0d6f9d0
mhbahmani <mhbahmani79@gmail.com>
2021-01-02 14:50:27 +0330

    add handling other distros support to install

--- install.sh
+++ install.sh
@@ -3,7 +3,34 @@ dollar_price_file="$data_backups_dir/dollar.txt"
 euro_price_file="$data_backups_dir/euro.txt"
 btc_price_file="$data_backups_dir/btc.txt"
 
-sudo apt install jq
+declare -A osInfo;
+osInfo[/etc/debian_version]="sudo apt-get install -y"
+osInfo[/etc/alpine-release]="sudo apk --update add"
+osInfo[/etc/centos-release]="sudo yum install -y"
+osInfo[/etc/fedora-release]="sudo dnf install -y"
+osInfo[/etc/manjaro-release]="sudo pamac install -y"
+
+for f in ${!osInfo[@]}
+do
+    if [[ -f $f ]];then
+        package_manager=${osInfo[$f]}
+    fi  
+done
+
+curl_inst=0
+[ $(curl --version | grep -Eci 'release-date') = 0 ] && curl_inst=1
+
+if [ $curl_inst = 1 ]; then
+    if [ -z $package_manager ]; then
+        echo "unknown linux distro".
+        echo "try installing jq with your package manager"
+        echo "then run the installation command again"
+        echo "exiting."
+        exit
+    fi
+    [ curl_intst = 1 ] && $package_manager jq
+fi
+
 
 rm -rf $data_backups_dir
 mkdir -p $data_backups_dir
@@ -20,3 +47,4 @@ EOF
 
 sudo chmod +x $main_script_file
 
+echo ""Done!""
