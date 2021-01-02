d6dd4444734099a27366c08738b7822f21d9d9b7
mhbahmani <mhbahmani79@gmail.com>
2021-01-02 14:37:07 +0330

    add btc to install.h

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 data_backups_dir="$HOME/.local/share/erate"
 dollar_price_file="$data_backups_dir/dollar.txt"
 euro_price_file="$data_backups_dir/euro.txt"
+btc_price_file="$data_backups_dir/btc.txt"
 
 sudo apt install jq
 
 rm -rf $data_backups_dir
-mkdir $data_backups_dir
-touch $dollar_price_file
-touch $euro_price_file
+mkdir -p $data_backups_dir
+touch $dollar_price_file $euro_price_file $btc_price_file
 
 erate_script=$(wget -q -O- https://raw.githubusercontent.com/mhbahmani/erate/master/erate)
 
